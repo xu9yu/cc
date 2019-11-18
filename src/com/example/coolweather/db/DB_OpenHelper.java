d3bc25b39202b050baaa64c0daf9a0e4cb17337b
unknown <5220170185@CODE-053.vdi.pri>
2019-11-18 16:17:25 +0800

    提交信息

---
+++ src/com/example/coolweather/db/DB_OpenHelper.java
@@ -0,0 +1,20 @@
+package com.example.coolweather.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DB_OpenHelper extends SQLiteOpenHelper {
+	public DB_OpenHelper(Context context,String name,CursorFactory factory,
+            int version){       
+        super (context,name,factory,version);   
+    }
+	public void onCreate(SQLiteDatabase db){
+		
+	}
+	public void onUpgrade(SQLiteDatabase db,int oldVersion,int newVersion){
+		
+	}
+
+}
